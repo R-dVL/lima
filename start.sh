83b524ca205ef3b2ac3eff25c3140ebb45670f05
Raul Del Valle Lima <rauldel.valle.lima@gmail.com>
2024-11-12 13:02:41 +0100

    Add migrations

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@rdvl-server.site', 'admin')
+    User.objects.create_superuser('admin', 'admin@rdvl.net', 'admin')
     print("Superuser created")
 else:
     print("Superuser already exists")
