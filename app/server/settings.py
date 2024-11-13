06f1022085ec9034e8bad503b8d5e0bafec1abd9
Raul Del Valle Lima <rauldel.valle.lima@gmail.com>
2024-11-13 14:03:06 +0100

    Add version

--- app/server/settings.py
+++ app/server/settings.py
@@ -15,9 +15,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+VERSION_FILE = os.path.join(BASE_DIR, 'VERSION')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+try:
+    with open(VERSION_FILE) as f:
+        APP_VERSION = f.read().strip()
+except FileNotFoundError:
+    APP_VERSION = "dev"
+
+def global_settings(request):
+    return {
+        'APP_VERSION': APP_VERSION,
+    }
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h6^c64$6540elqq_ngu6)nnpa8)570ty)au7wjmjs8=nh=*qts'
@@ -66,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'server.settings.global_settings'
             ],
         },
     },
