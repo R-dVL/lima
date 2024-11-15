64802cbe53711a58dc160e5f68062b4a8487872e
Raul Del Valle Lima <rauldel.valle.lima@gmail.com>
2024-11-15 21:17:55 +0100

    Update urls

--- app/inventory/urls.py
+++ app/inventory/urls.py
@@ -9,12 +9,12 @@ from . import views
 
 urlpatterns = [
     path('', views.lists, name='lists'),
-    path('increase/<int:pk>/', views.increase_amount, name='increase_amount'),
-    path('decrease/<int:pk>/', views.decrease_amount, name='decrease_amount'),
-    path('create/<int:pk>/', views.ItemCreateView.as_view(), name='create_item'),
-    path('update/<int:pk>', views.ItemUpdateView.as_view(), name='update_item'),
-    path('read/<int:pk>', views.ItemReadView.as_view(), name='read_item'),
-    path('delete/<int:pk>', views.ItemDeleteView.as_view(), name='delete_item'),
+    path('item/increase/<int:pk>/', views.increase_amount, name='increase_amount'),
+    path('item/decrease/<int:pk>/', views.decrease_amount, name='decrease_amount'),
+    path('item/create/<int:pk>/', views.ItemCreateView.as_view(), name='create_item'),
+    path('item/update/<int:pk>', views.ItemUpdateView.as_view(), name='update_item'),
+    path('item/read/<int:pk>', views.ItemReadView.as_view(), name='read_item'),
+    path('item/delete/<int:pk>', views.ItemDeleteView.as_view(), name='delete_item'),
     path('lists/', views.lists, name='lists'),
     path('lists/<int:pk>/', views.list_detail, name='list_detail')
 ]
